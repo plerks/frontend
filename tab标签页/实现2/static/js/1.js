aee048ef48483c15aa07a36957627741e5da54e8
plerks <plerks@163.com>
2022-08-11 19:38:43 +0800

    "tab标签页"实现2, TabItem高度不相同时tab-container高度可能不贴合TabItem的问题改用provide, inject来控制

--- tab标签页/实现2/static/js/1.js
+++ tab标签页/实现2/static/js/1.js
@@ -73,6 +73,7 @@ const rippleButton = {
         }
     }
 }
+const { computed } = Vue;
 const tab = {
     components: {
         RippleButton: rippleButton
@@ -80,6 +81,11 @@ const tab = {
     data() {
         return {
             activeIndex: 0
+        }
+    },
+    provide() {
+        return {
+            activeIndex: computed(() => this.activeIndex)
         }
     },
     computed: {
@@ -105,9 +111,7 @@ const tab = {
             <div style="position: relative">
                 <div class="tab-container"
                   :style="{width: this.tabHeaders.length * 100 +'%',transform: this.tabTranslateX}">
-                  <template v-for="(header,index) in tabHeaders">
-                    <slot :name="index" :values="activeIndex==index?'':'tab-item-inactive'"></slot>
-                  </template>
+                  <slot></slot>
                 </div>
             </div>
         </div>
@@ -115,8 +119,10 @@ const tab = {
     `
 }
 const tabItem = {
+    inject: ['activeIndex'],
+    props: ['index'],
     template: `
-        <div class="tab-item">
+        <div :class="{'tab-item':true, 'tab-item-inactive': this.activeIndex.value != this.index}">
             <slot></slot>
         </div>
     `
@@ -137,32 +143,31 @@ const app = Vue.createApp({
     },
     template: `
     <Tab :tabHeaders="this.tabHeaders">
-        <!-- 要求Tab组件用户写上template和:class="slotProps.values", 用来控制.tab-item-inactive类 -->
-        <template #0="slotProps">
-            <TabItem :class="slotProps.values">
-                <div class="card" style="border-color: rgb(151, 233, 151)">1</div>
-            </TabItem>
-        </template>
-        <template #1="slotProps">
-            <TabItem :class="slotProps.values">
-                <div class="card" style="border-color: red">2</div>
-                <div class="card" style="border-color: red">2</div>
-                <div class="card" style="border-color: red">2</div>
-            </TabItem>
-        </template>
-        <template #2="slotProps">
-            <TabItem :class="slotProps.values">
-                <div class="card" style="border-color: blue">3</div>
-            </TabItem>
-        </template>
+        <!-- 要求Tab组件用户写上:index="0",用来控制tab-item-inactive类 -->
+        <TabItem :index="0">
+            <div class="card" style="border-color: rgb(151, 233, 151)">1</div>
+        </TabItem>
+        <TabItem :index="1">
+            <div class="card" style="border-color: red">2</div>
+            <div class="card" style="border-color: red">2</div>
+            <div class="card" style="border-color: red">2</div>
+        </TabItem>
+        <TabItem :index="2">
+            <div class="card" style="border-color: blue">3</div>
+        </TabItem>
     </Tab>
 `
 }).mount("#app");
 
+
 /*
 为了便于使用Tab组件,具体的Tab中的内容需要由Tab的父组件提供给Tab中的插槽。
-此外,非在显示的TabItem需要设置height为0,否则tab-container的高度将由最高的TabItem决定,会导致较矮的TabItem下方有空白,
-而不是tab-container贴合当前的TabItem。
-但是,只有Tab才知道active的TabItem,所以需要用作用域插槽(scoped slots)从Tab的<slot>向父组件的<template>传递数据,然后再给
-到TabItem改变height
+有个特殊点是非显示状态的TabItem需要设置height为0,否则tab-container的高度将由最高的TabItem决定,会导致较矮的TabItem下方有空白,而不是tab-container贴合当前的TabItem。
+但是,只有Tab组件才知道active的TabItem,所以Tab组件里需要有个provide, inject到TabItem里,控制TabItem的高度。
+响应式的provide的写法见https://cn.vuejs.org/guide/components/provide-inject.html#working-with-reactivity, "../../common/js/vue3/vue.global.js"的版本稍微有点旧似乎是3.2.31,
+activeIndex取值时还需要加上.value, 浏览器控制台也有提示：
+    vue.global.js:1568 [Vue warn]: injected property "activeIndex" is a ref and will be auto-unwrapped and no longer needs `.value` in the next minor release.
+    To opt-in to the new behavior now, set `app.config.unwrapInjectedRef = true` (this config is temporary and will not be needed in the future.)
+
+除了用provide,inject, 还有一种办法是用作用域插槽,见commit:5ca9d8b3727b3b48815174b0e07464f4b3bc8b73
 */
