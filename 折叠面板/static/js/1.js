feeb268bc830de00d33c4dc43f07f91f0f234911
plerks <plerks@163.com>
2022-09-12 20:46:13 +0800

    修复"折叠面板"动画被打断时开闭不连续的问题 之前如果折叠面板动画打开到一半点关闭，会直接突然变成完全打开的高度，然后动画关闭到0。 用了个currentHeight属性，记录发生click，要进行动画时当前的实际高度。 找到了element UI库的源码，但是没看出来他们解决这个问题的关键点。

--- 折叠面板/static/js/1.js
+++ 折叠面板/static/js/1.js
@@ -1,14 +1,15 @@
 const collapse = {
     data() {
         return {
-            expanded: false
+            expanded: false,
+            currentHeight: 0
         }
     },
     template: `
 <div class="card" style="width: 300px;padding: 5px">
     <div class="container">
         <div class="row" style="border: 1px solid #dedede;color: #79848b;font-family: Arial, Helvetica;">
-<!--        这里可以用.has-arrow:not(.expanded)::after和.has-arrow.expanded::after来画箭头并控制旋转，但一般都是用aria-expanded属性来控制旋转
+            <!--这里可以用.has-arrow:not(.expanded)::after和.has-arrow.expanded::after来画箭头并控制旋转，但一般都是用aria-expanded属性来控制旋转
             <span class="has-arrow" :class="{expanded:expanded}" @click="expand()" style="flex: 1 1;padding: 5px;color: #5f5f5f">点击</span>-->
             <span class="has-arrow" :aria-expanded="expanded" @click="expand()" style="flex: 1 1;padding: 5px;color: #5f5f5f">点击</span>
         </div>
@@ -16,10 +17,14 @@ const collapse = {
     <div style="overflow: hidden">
         <transition
             name="fade-in"
+            @beforeEnter="beforeEnter"
             @enter="enter"
+            @afterEnter="afterEnter"
+            @beforeLeave="beforeLeave"
             @leave="leave"
+            @afterLeave="afterLeave"
         >
-          <div v-if="expanded">
+          <div v-show="expanded" ref="content">
             <button class="button">Collapse Content</button>
             <button class="button">Collapse Content</button>
             <button class="button">Collapse Content</button>
@@ -36,18 +41,37 @@ const collapse = {
     methods: {
         expand: function () {
             this.expanded = !this.expanded
+            this.currentHeight = this.$refs.content.offsetHeight
+        },
+        beforeEnter(el) {
+            el.style.height = ''
         },
         enter(el) {
             let expandedHeight = window.getComputedStyle(el).height
-            el.style.height = '0px'
+            el.style.height = this.currentHeight + 'px'
             el.offsetHeight // 触发浏览器回流
             el.style.height = expandedHeight // 要展开不能设置height为auto，不会出动画，所以要先获得固定值的高度
         },
+        afterEnter(el) {
+            el.style.height = ''
+        },
+        beforeLeave(el) {
+            el.style.height = ''
+        },
         leave (el) {
-            //el.style.height = window.getComputedStyle(el).height
-            el.offsetHeight // 触发浏览器回流，这里可以不用触发，上面enter()需要触发回流，见"../../说明.md"，应该只需要保证渲染线程看到高度从固定值到固定值的变化就行
+            el.style.height = this.currentHeight + 'px'
+            el.offsetHeight // 触发浏览器回流
             el.style.height = '0px'
         },
+        afterLeave(el) {
+            el.style.height = ''
+        }
+
+        /*
+        上面的v-show="expanded"不能换成v-if="expanded"。否则会有问题，毕竟v-if要重新插入元素到dom中，所以这里用v-if动画会不正常。
+        为了保证动画被打断时的开闭连续(打开到一半时点关闭，要保证从半高缩到0，而非突然变成全高再缩到0)，必须获得目前变化到的高度。
+        所以要在click时获得当前的高度并记录，然后动画变化到0/全高。
+        */
     }
 }
 const app = Vue.createApp({
